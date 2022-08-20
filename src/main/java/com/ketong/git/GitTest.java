a321b4af910ba7610db9e602888d29a2438d1920
Ketong <zhanghuadongemail@163.com>
2022-08-20 23:39:29 +0800

    hot-fix commit2

--- src/main/java/com/ketong/git/GitTest.java
+++ src/main/java/com/ketong/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello world222!");
         System.out.println("hello world333!");
         System.out.println("hello world hot-fix444!");
+        System.out.println("hot-fix test!");
     }
 }
